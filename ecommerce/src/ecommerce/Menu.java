de3d9e16622d86abb9cbeb5b1acb20bd586b1b87
Ingrid Alkimim <ingridalkm@outlook.com>
2024-05-08 09:35:58 -0300

    Menu

---
+++ ecommerce/src/ecommerce/Menu.java
@@ -0,0 +1,122 @@
+package ecommerce;
+
+import java.util.InputMismatchException;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Menu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner leia = new Scanner(System.in);
+
+		int opcao = 0;
+
+		while (true) {
+			System.out.println("----------------------------------------------------");
+			System.out.println("                                                   |");
+			System.out.println("                    E-GEN                          |");
+			System.out.println("           O melhor E-commerce do Brasil           |");
+			System.out.println("                                                   |");
+			System.out.println("----------------------------------------------------");
+			System.out.println("                                                   |");
+			System.out.println("           1 - Listar todos os produtos            |");
+			System.out.println("           2 - Listar por ID			   |");
+			System.out.println("           3 - Cadastrar produto		   |");
+			System.out.println("           4 - Atualizar produto		   |");
+			System.out.println("           5 - Deletar produto			   |");
+			System.out.println("           6 - Sair			   |");
+			System.out.println("----------------------------------------------------");
+			System.out.println("                                                   |");
+			System.out.println("Entre com a opção desejada:                        |");
+			System.out.println("                                                   |");
+
+			try {
+				opcao = leia.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("\nDigite valores inteiros!");
+				leia.nextLine();
+				opcao = 0;
+			}
+			
+			if(opcao == 6) {
+				System.out.println("\nE-GEN - O melhor E-commerce do Brasil!");
+				sobre();
+				leia.close();
+				System.exit(0);
+				
+			}
+			
+			switch(opcao) {
+			
+			case 1->{
+				System.out.println("\nLista de todos os produtos\n");
+				
+				keyPress();
+				break;
+				
+			}
+			
+			case 2->{
+				System.out.println("\nListar por ID\n");
+				
+				keyPress();
+				break;
+				
+			}
+			
+			case 3->{
+				System.out.println("\nCadastrar Produto\n");
+				
+				keyPress();
+				break;
+				
+			}
+			
+			case 4->{
+				System.out.println("\nAtualizar produto\n");
+				
+				keyPress();
+				break;
+				
+			}
+			
+			case 5->{
+				System.out.println("\nDeletar produto");
+				
+				keyPress();
+				break;
+				
+			}
+			
+			
+			}
+			
+
+		}
+	}
+	
+	
+	public static void sobre() {
+		System.out.println("\n*********************************************************");
+		System.out.println("Projeto Desenvolvido por: ");
+		System.out.println("Ingrid Alkimim");
+		System.out.println("*********************************************************");
+	}
+
+	public static void keyPress() {
+
+		try {
+
+			System.out.println("\n\nPressione Enter para Continuar...");
+			System.in.read();
+
+		} catch (IOException e) {
+
+			System.out.println("Você pressionou uma tecla diferente de enter!");
+
+		}
+	}
+
+}
